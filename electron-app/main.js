40e89f20680a0f829184476419982d8210011e17
besiktaş1903 <acaroktay1967@gmail.com>
2026-05-22 20:01:30 +0300

    Electron 'main.js' dosyasını güncelliyoruz: kısa karar metni eklenmiştir.

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,6 +1,20 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+// Karar metni oluşturma için örnek fonksiyon
+function generateKararMetni() {
+  return `Mahkemece yapılan değerlendirmeler neticesinde:
+
+Sanık hakkında takdiren 2 yıl hapis cezası verilmiştir.
+
+Uygulanan Maddeler:
+ - TCK 51: Hapis cezasının ertelenmesi ve koşulları.
+ - TCK 53: Belirli hak yoksunluklarının uygulanması.
+ - TCK 58: Tekerrür hükümleri.
+ - CMK 231: Hükmün açıklanmasının geri bırakılması.
+`;
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1440,
@@ -14,7 +28,17 @@ function createWindow() {
     }
   });
 
-  win.loadFile(path.join(__dirname, '..', 'index.html'));
+  // Karar metnini almak ve bir HTML dosyasına iletmek
+  const kararMetni = generateKararMetni();
+  win.loadURL('data:text/html;charset=utf-8,' + encodeURIComponent(`
+    <html>
+    <head><title>Kısa Karar</title></head>
+    <body>
+      <h1>Kısa Karar</h1>
+      <pre>${kararMetni}</pre>
+    </body>
+    </html>
+  `));
 }
 
 app.whenReady().then(() => {
